407f151a14c935a5913158a53f4c1172d3d6da68
Tanvir <donlol1845@gmail.com>
2019-04-19 16:56:54 +0600

    11044 - Searching for Nessy

---
+++ UVA/UVA 11044 - Searching for Nessy.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	while(n--)
+	{
+		int a,b;
+		cin >> a >> b;
+
+		cout << (int)((a)/3) * (int)((b)/3) << endl;
+	}
+
+	return 0;
+}
